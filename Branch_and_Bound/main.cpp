456f666153e03b91408d43c3e0c7afeb04ee4abf
Jshewmaker <jshewmak@gmail.com>
2017-12-04 12:01:55 -0500

    Initial Commit

---
+++ Branch_and_Bound/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Branch_and_Bound
+//
+//  Created by Joshua Shewmaker on 12/4/17.
+//  Copyright © 2017 Joshua Shewmaker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
